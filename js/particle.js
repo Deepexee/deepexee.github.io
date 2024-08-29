6dd9163b45246a9403e64639221f04b14389c4cf
Deepexe <79488121+Deepexee@users.noreply.github.com>
2024-08-29 09:23:37 +0200

    Fix particle bug

--- js/particle.js
+++ js/particle.js
@@ -2,10 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
     function createParticle(x, y) {
         const particle = document.createElement('div');
         particle.classList.add('particle');
+
+        let deltaX = (Math.random() - 0.5) * 100;
+        let deltaY = (Math.random() - 0.5) * 100;
+        if (x + deltaX > window.innerWidth || x + deltaX < 0) {
+            deltaX = -deltaX;
+        }
+        if (y + deltaY > window.innerHeight || y + deltaY < 0) {
+            deltaY = -deltaY;
+        }
+
         particle.style.left = `${x}px`;
         particle.style.top = `${y}px`;
-        particle.style.setProperty('--x', `${(Math.random() - 0.5) * 100}px`);
-        particle.style.setProperty('--y', `${(Math.random() - 0.5) * 100}px`);
+        particle.style.setProperty('--x', `${deltaX}px`);
+        particle.style.setProperty('--y', `${deltaY}px`);
         document.body.appendChild(particle);
         
         setTimeout(() => {
